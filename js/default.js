0bcc2ef2212afb5919c1700cc808595d7ed07d51
srikanthmannepalle <smannepalle@mobileiron.com>
2015-12-20 02:12:05 +0530

    Comments section implemented

---
+++ js/default.js
@@ -0,0 +1,9 @@
+/**
+ * Created by srikanthmannepalle on 12/19/15.
+ */
+
+$(document).ready(function(){
+    $('[data-toggle="offCanvas"]').click(function(){
+        $('#side-menu').toggleClass('hidden-xs')
+    });
+});
